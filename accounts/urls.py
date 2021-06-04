379e73b609f66ea0c6f25b2e4fb627a5663c3c4e
Kirill <61096456+bedkir@users.noreply.github.com>
2021-06-04 23:02:19 +0300

    cart

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import register, login, logout, userProfilePage
+from .views import register, login, logout, userProfilePage, userOrders, userCart, userFavourites
 
 urlpatterns = [
     path('register/', register),
     path('login/', login),
     path('logout/', logout),
-    path('<int:uid>/', userProfilePage)
+    path('<int:uid>/', userProfilePage),
+    path('<int:uid>/orders', userOrders),
+    path('<int:uid>/cart', userCart),
+    path('<int:uid>/favourites', userFavourites)
 ]
