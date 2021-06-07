acc4d620491af5d65dd113148691da2540223918
Kirill <61096456+bedkir@users.noreply.github.com>
2021-06-07 21:02:39 +0300

    gfj

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.template.context_processors import csrf
 from .models import AuthUser, Продукт
 from .forms import SignUpForm
 from django.db import models
+from django.db.models import F
 
 products = Продукт.objects.all()
 accounts = AuthUser.objects.all()
@@ -86,9 +87,21 @@ def userCart(request, uid):
     #DELETE FROM CART
     if request.POST:
         itemToDelete = request.POST.get('delete', '')
-        addOneMore = request.POST.get('add', '')
-        removeOneMore = request.POST.get('remove', '')
-        ShopCarty.objects.filter(item=itemToDelete).delete()
+        addOneMore = request.POST.get('plus', '')
+        removeOneMore = request.POST.get('minus', '')
+        #carts = ShopCarty.objects.all()
+        if addOneMore:
+            item = request.POST.get('item_plus', '')
+            carts = ShopCarty.objects.get(item=item)
+            carts.amount += 1
+            carts.save()                    
+        elif removeOneMore:
+            item = request.POST.get('item_minus', '')
+            carts = ShopCarty.objects.get(item=item)
+            carts.amount -= 1
+            carts.save()
+        elif itemToDelete:
+            ShopCarty.objects.filter(item=itemToDelete).delete()
         return redirect('/accounts/' + str(request.user.id) + '/cart')
     else:
         carts = ShopCarty.objects.all()
