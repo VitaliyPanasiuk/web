c3e0788e1aaee68323ca1d7c59d427deabcd82a5
Kirill <61096456+bedkir@users.noreply.github.com>
2021-09-01 15:51:16 +0300

    qeg

--- shop/views.py
+++ shop/views.py
@@ -421,7 +421,7 @@ def aboutProductPage(request, id, lang):
             db_table = 'shop_favourite'
         
     '''for i in продукты:
-        if int(i.id) > 89:
+        if int(i.id) > 109:
             russian_desc = i.описание
             from googletrans import Translator
             translator = Translator(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
